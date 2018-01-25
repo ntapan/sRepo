725a3f91a9bbf911165c9d672bef832e9a77588d
nt <nt@192.168.186.142>
2018-01-25 10:36:29 +0300

    oıuıy

--- sProject/src/sProject/Hello.java
+++ sProject/src/sProject/Hello.java
@@ -3,7 +3,7 @@ package sProject;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("hrldddo");
+		System.out.println("hrlddoýdo");
 		System.out.println("eee2de");
 		System.out.println("hrlo");
 		System.out.println("ee777ee");
