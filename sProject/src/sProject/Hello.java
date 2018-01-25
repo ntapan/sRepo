9038d401757a03381c326ee92a925529fd22211a
nt <nt@192.168.186.142>
2018-01-25 10:49:21 +0300

    g

--- sProject/src/sProject/Hello.java
+++ sProject/src/sProject/Hello.java
@@ -6,8 +6,7 @@ public class Hello {
 		
 		System.out.println("uuuuu");
 		System.out.println("eee2de");
-		System.out.println("11111");
-		System.out.println("ee777ee");
+		System.out.println("rrr");
 	}
 
 }
