4d4ec8c7c79e9d2b24a7f133f7afbf7d7414c309
nt <nt@192.168.186.142>
2018-01-25 10:32:08 +0300

    xc

--- sProject/src/sProject/Hello.java
+++ sProject/src/sProject/Hello.java
@@ -4,7 +4,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("hrldddo");
-		System.out.println("eee2e");
+		System.out.println("eee2de");
+		System.out.println("hrlo");
+		System.out.println("eeee");
 	}
 
 }
